b68a35c515e4e74c89c9a3d5decec8f17b121eaa
thousifive <thousif4023@gmail.com>
2022-04-19 01:27:22 +0530

    rendered basic map and tile layer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import React, { useState } from "react";
 import "./App.css";
+import Map from "./Map/Map";
+import Layers from "./Layers/Layers";
+import TileLayer from "./Layers/TileLayer";
+import VectorLayer from "./Layers/VectorLayer";
+import { osm, vector } from "./Source";
+import Controls from "./Controls/Controls";
+import FullScreenControl from "./Controls/FullScreenControl";
 
-function App() {
+const App = () => {
+  const [center, setCenter] = useState([0, 0]);
+  const [zoom, setZoom] = useState(2);
   return (
-    <div className="App">
-      <header className="App-header">React + OpenLayers</header>
+    <div>
+      <Map center={center} zoom={zoom}>
+        <Layers>
+          <TileLayer source={osm()} zIndex={0} />
+        </Layers>
+        <Controls>
+          <FullScreenControl />
+        </Controls>
+      </Map>
     </div>
   );
-}
+};
 
 export default App;
